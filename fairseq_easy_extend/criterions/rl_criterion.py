f75551435e2b3ad5bd5efda07bb2972cedb2213a
Rachel van t Hull <46531228+remvanthull@users.noreply.github.com>
2023-04-19 15:59:53 +0200

    added bert

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -9,14 +9,12 @@ from torch import Tensor
 
 from dataclasses import dataclass, field
 import sacrebleu
-
+from bert_score import score as bert_score
 
 @dataclass
 class RLCriterionConfig(FairseqDataclass):
     sentence_level_metric: str = field(default="bleu",
                                        metadata={"help": "sentence level metric"})
-
-
 
 
 @register_criterion("rl_loss", dataclass=RLCriterionConfig)
@@ -25,10 +23,9 @@ class RLCriterion(FairseqCriterion):
         super().__init__(task)
         self.metric = sentence_level_metric
         self.tgt_dict = task.target_dictionary
+        self.tgt_lang = "en"
 
-    def _compute_loss(
-        self, outputs, targets, masks=None, label_smoothing=0.0, name="loss", factor=1.0
-    ):
+    def _compute_loss(self, outputs, targets, masks=None):
         """
         outputs: batch x len x d_model
         targets: batch x len
@@ -38,7 +35,6 @@ class RLCriterion(FairseqCriterion):
         #padding mask, do not remove
         if masks is not None:
             outputs, targets = outputs[masks], targets[masks]
-        
 
         #we take a softmax over outputs
         #argmax over the softmax \ sampling (e.g. multinomial)
@@ -50,7 +46,7 @@ class RLCriterion(FairseqCriterion):
             #R(*) = eval_metric(sampled_sentence_string, target_sentence)
             #R(*) is a number, BLEU, сhrf, etc.
 
-        #loss = -log_prob(outputs)*R()
+        #loss = -log_prob(sample_outputs)*R()
         #loss = loss.mean()
 
         log_probs = torch.nn.functional.log_softmax(outputs, dim=-1)
@@ -60,9 +56,12 @@ class RLCriterion(FairseqCriterion):
         target_str = self.tgt_dict.string(targets)
         with torch.no_grad():
             if self.metric == "bleu":
-                score = sacrebleu.sentence_bleu(predicted_str, [target_str], smooth_method="exp", smooth_value=label_smoothing).score
+                score = sacrebleu.sentence_bleu(predicted_str, [target_str]).score
             elif self.metric == "chrf":
                 score = sacrebleu.sentence_chrf(predicted_str, [target_str]).score
+            elif self.metric == "bert":
+                _, _, score = bert_score([predicted_str], [target_str], lang=self.tgt_lang)
+                score = score.mean()
       
         sample_log_probs = torch.gather(log_probs, 1, predicted.unsqueeze(1)).squeeze()
         loss = -sample_log_probs * score
@@ -88,9 +87,10 @@ class RLCriterion(FairseqCriterion):
 
         outputs = model(src_tokens, src_lengths, prev_output_tokens, tgt_tokens)
         #get loss only on tokens, not on lengths
-        outputs = outputs["word_ins"]
-        masks = outputs.get("mask", None),
-        loss = self._compute_loss(outputs, tgt_tokens, masks)
+        outs = outputs["word_ins"].get("out", None)
+        masks = outputs["word_ins"].get("mask", None)
+
+        loss = self._compute_loss(outs, tgt_tokens, masks)
 
         # NOTE:
         # we don't need to use sample_size as denominator for the gradient
@@ -104,4 +104,40 @@ class RLCriterion(FairseqCriterion):
             "sample_size": sample_size,
         }
 
-        return loss, sample_size, logging_output
+    @staticmethod
+    def reduce_metrics(logging_outputs) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        sample_size = utils.item(
+            sum(log.get("sample_size", 0) for log in logging_outputs)
+        )
+        loss = utils.item(sum(log.get("loss", 0) for log in logging_outputs))
+        nll_loss = utils.item(sum(log.get("nll_loss", 0) for log in logging_outputs))
+
+        metrics.log_scalar(
+            "loss", loss / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_scalar(
+            "nll_loss", nll_loss / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_derived(
+            "ppl", lambda meters: utils.get_perplexity(meters["loss"].avg)
+        )
+
+        for key in logging_outputs[0]:
+            if key[-5:] == "-loss":
+                val = sum(log.get(key, 0) for log in logging_outputs)
+                metrics.log_scalar(
+                    key[:-5],
+                    val / sample_size / math.log(2) if sample_size > 0 else 0.0,
+                    sample_size,
+                    round=3,
+                )
+
+    @staticmethod
+    def logging_outputs_can_be_summed() -> bool:
+        """
+        Whether the logging outputs returned by `forward` can be summed
+        across workers prior to calling `reduce_metrics`. Setting this
+        to True will improves distributed training speed.
+        """
+        return True
